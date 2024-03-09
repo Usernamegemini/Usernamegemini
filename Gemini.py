b2dc5fb30db4c5bdc604cd1208b97f1d3e47d808
Usernamegemini <162820942+Usernamegemini@users.noreply.github.com>
2024-03-09 21:28:18 +0530

    Create Gemini.py
    It is an Gemini ai using the Generativeai libary

---
+++ Gemini.py
@@ -0,0 +1,35 @@
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyBb_fuvn50S7nS80FXOrSUv68sT1DZeyVw")
+generation_config = {
+  "temperature": 0.5,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_LOW_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_ONLY_HIGH"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+convo = model.start_chat(history=[])
+input = "give me 2 diffucult formula in vector for class 12"
+convo.send_message(input)
+output = convo.last.text
